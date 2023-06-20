225a011f7520a56c71ac7c8564c18c90a7602c62
rodrigo.baggio.si@gmail.com <Baggio25>
2023-06-20 11:37:19 -0300

    Fonts and Colors, Default Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { ThemeProvider } from "styled-components";
+
+import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
-  return <div>Timer</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
---
+++ src/components/Button.styles.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+
+export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
+
+interface ButtonContainerProps {
+    variant: ButtonVariant;    
+}
+
+const buttonVariants = {
+    primary: 'purple',
+    secondary: 'gray',
+    danger: 'red',
+    success: 'green'
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+  border-radius: 4px;
+  border: 0;
+  margin: 8px;
+  color: white;
+  cursor: pointer;
+
+  background-color: ${props => props.theme["green-500"]};
+
+`;
