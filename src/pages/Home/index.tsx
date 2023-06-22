82149d4a497d0f6a4fad2b5a1bc0842871e18145
rodrigo.baggio.si@gmail.com <Baggio25>
2023-06-22 10:47:40 -0300

    Inputs improves

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,10 +8,28 @@ export function Home() {
       <form>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
-          <TaskInput id="task" autoFocus placeholder="Dê um nome para o seu projeto"/>
+          <TaskInput 
+            id="task" 
+            list="task-suggestions"
+            autoFocus 
+            placeholder="Dê um nome para o seu projeto"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1" />
+            <option value="Projeto 2" />
+            <option value="Projeto 3" />
+            <option value="Projeto 4" />
+          </datalist>
 
           <label htmlFor="minutesAmount">durante</label>
-          <MinutesAmountInput id="minutesAmount" type="number" placeholder="00" />
+          <MinutesAmountInput 
+            id="minutesAmount" 
+            type="number" 
+            placeholder="00" 
+            step={5} min={5} 
+            max={60} 
+          />
 
           <span>minutos.</span>
         </FormContainer>
