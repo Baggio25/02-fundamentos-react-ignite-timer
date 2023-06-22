06530c7a735bb427e18ee7e57c7b8f10cc94b6fa
rodrigo.baggio.si@gmail.com <Baggio25>
2023-06-22 11:30:27 -0300

    Page History

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
     max-width: 74rem;
-    height: calc(100vh - 9rem);
+    min-height: calc(100vh - 10rem);
     margin: 4.5rem auto;
     padding: 2.5rem;
     gap: 1rem;
