086f6b1edba7a71df949b534aaef880a04126998
rodrigo.baggio.si@gmail.com <Baggio25>
2023-06-22 10:38:46 -0300

    Finish Home

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -2,9 +2,10 @@ import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
     max-width: 74rem;
-    height: calc(100vh - 10rem);
-    margin: 5rem auto;
+    height: calc(100vh - 9rem);
+    margin: 4.5rem auto;
     padding: 2.5rem;
+    gap: 1rem;
 
     display: flex;
     flex-direction: column;
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -11,7 +11,7 @@ export const HomeContainer = styled.main`
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 3.5rem;
+    gap: 2rem;
   }
 `;
 
@@ -25,7 +25,37 @@ export const FormContainer = styled.div`
   font-weight: bold;
   font-size: 1.125rem;
   flex-wrap: wrap;
+  color: ${(props) => props.theme["gray-100"]};
+`;
+
+const BaseInput = styled.input`
+  height: 2.5rem;
+  border: 0;
+
+  font-weight: bold;
+  font-size: inherit;
+  padding: 0 0.5rem;
+
   color: ${props => props.theme["gray-100"]};
+  border-bottom: 2px solid ${props => props.theme["gray-400"]};
+  background: transparent;
+  
+  &:focus {
+    box-shadow: none;
+    border-color: ${props => props.theme["green-500"]};;
+  }
+
+  &::placeholder {
+    color: ${props => props.theme["gray-500"]};
+  }
+`;
+
+export const TaskInput = styled(BaseInput)`
+  flex: 1;
+`;
+
+export const MinutesAmountInput = styled(BaseInput)`
+  width: 4rem;
 `;
 
 export const CountdownContainer = styled.div`
@@ -35,12 +65,12 @@ export const CountdownContainer = styled.div`
 
   display: flex;
   gap: 1rem;
-  color: ${props => props.theme["gray-100"]};
+  color: ${(props) => props.theme["gray-100"]};
 
   span {
     padding: 2rem 1rem;
     border-radius: 8px;
-    background: ${props => props.theme["gray-700"]};
+    background: ${(props) => props.theme["gray-700"]};
   }
 `;
 
@@ -51,5 +81,32 @@ export const Separator = styled.div`
   display: flex;
   justify-content: center;
 
-  color: ${props => props.theme["green-500"]};
+  color: ${(props) => props.theme["green-500"]};
+`;
+
+export const StartCountdownButton = styled.button`
+  width: 100%;
+  border: 0;
+  padding: 1rem;
+  border-radius: 8px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  gap: 0.5rem;
+  font-weight: bold;
+  cursor: pointer;
+
+  background: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme["gray-100"]};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme["green-700"]};
+  }
+
+  &:disabled {
+    opacity: 0.7;
+    cursor: not-allowed;
+  }
 `;
