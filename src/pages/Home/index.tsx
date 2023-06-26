bb2aed298d34bd2b5e030c8f23ddbc91084a1d7e
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2023-06-26 09:50:56 -0300

    Alter page title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -47,11 +47,18 @@ export function Home() {
   const activeCycle = cycles.find(cycle => cycle.id === activeCycleId);
 
   useEffect(() => {
+    let interval: number;
+
     if(activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
       }, 1000);
     }
+
+    return () => {
+      clearInterval(interval);
+    }
+
   },[activeCycle]);
 
   function handleCreateNewCycle(data: NewCycleFormData) {
@@ -64,6 +71,7 @@ export function Home() {
 
     setCycles(state => [...state, newCycle]);
     setActiveCycleId(newCycle.id);
+    setAmountSecondsPassed(0);
 
     reset();
   }
@@ -77,6 +85,13 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0');
   const seconds = String(secondsAmount).padStart(2, '0');
+
+  useEffect(() => {
+    if(activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle]);
+
 
   const task = watch("task");
   const isSubmitDisabled = !task;
