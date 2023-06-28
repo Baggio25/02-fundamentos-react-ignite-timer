598c59d1fdf341aede2714697e0917ab7bf6a8af
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2023-06-28 09:05:48 -0300

    Finished cycle

--- src/context/CyclesContext.tsx
+++ src/context/CyclesContext.tsx
@@ -39,35 +39,59 @@ interface CyclesState {
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cycles, dispatch] = useReducer((state: CyclesState, action: any) => {
-    if (action.type === "ADD_NEW_CYCLE") {
-      return {
-        ...state, 
-        cycles: [...state.cycles, action.payload.newCycle]
-      };
+  const [cyclesState, dispatch] = useReducer(
+    (state: CyclesState, action: any) => {
+      if (action.type === "ADD_NEW_CYCLE") {
+        return {
+          ...state,
+          cycles: [...state.cycles, action.payload.newCycle],
+          activeCycleId: action.payload.newCycle.id,
+        };
+      }
+
+      if (action.type === "INTERRUPT_CURRENT_CYCLE") {
+        return {
+          ...state,
+          cycles: state.cycles.map((cycle) => {
+            if (cycle.id === state.activeCycleId) {
+              return { ...cycle, interruptedDate: new Date() };
+            } else {
+              return cycle;
+            }
+          }),
+          activeCycleId: null,
+        };
+      }
+      
+      if (action.type === "MARK_CURRENT_CYCLE_AS_FINISHED") {
+        return {
+          ...state,
+          cycles: state.cycles.map((cycle) => {
+            if (cycle.id === state.activeCycleId) {
+              return { ...cycle, finishedDate: new Date() };
+            } else {
+              return cycle;
+            }
+          }),
+          activeCycleId: null,
+        };
+      }
+
+
+      return state;
+    },
+    {
+      cycles: [],
+      activeCycleId: null,
     }
+  );
 
-    return state;
-  }, {
-    cycles: [],
-    activeCycleId: null
-  });
-
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
+  const { cycles, activeCycleId } = cyclesState;
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   function markCurrencyCycleAsFinished() {
-    //setCycles((state) =>
-    //  state.map((cycle) => {
-    //    if (cycle.id === activeCycleId) {
-    //      return { ...cycle, finishedDate: new Date() };
-    //    } else {
-    //      return cycle;
-    //    }
-    //  })
-    //);
 
     dispatch({
       type: "MARK_CURRENT_CYCLE_AS_FINISHED",
@@ -96,8 +120,6 @@ export function CyclesContextProvider({
       },
     });
 
-    //setCycles((state) => [...state, newCycle]);
-    setActiveCycleId(newCycle.id);
     setAmountSecondsPassed(0);
   }
 
@@ -108,18 +130,6 @@ export function CyclesContextProvider({
         activeCycleId,
       },
     });
-
-    //setCycles((state) =>
-    //  state.map((cycle) => {
-    //    if (cycle.id === activeCycleId) {
-    //      return { ...cycle, interruptedDate: new Date() };
-    //    } else {
-    //      return cycle;
-    //    }
-    //  })
-    //);
-
-    setActiveCycleId(null);
   }
 
   return (
