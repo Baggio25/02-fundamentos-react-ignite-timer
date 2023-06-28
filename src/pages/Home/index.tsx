e24f56845fe4a642d4665de381d8757f95cef806
Rodrigo Baggio <rodrigo.baggio.si@gmail.com>
2023-06-28 08:19:43 -0300

    Date Format

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -18,7 +18,7 @@ const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
   minutesAmount: zod
     .number()
-    .min(1, "O ciclo precisa ser de no mínimo 5min")
+    .min(5, "O ciclo precisa ser de no mínimo 5min")
     .max(60, "O ciclo precisa ser de no máximo 60min"),
 });
 
